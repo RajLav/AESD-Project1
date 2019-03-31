b80bae16ac3191c670788824cff63e869d836a99
yash1595 <yashmgupte@gmail.com>
2019-03-31 02:06:40 -0600

    Unit Test - Logger

--- UnitTesting/LoggerUnitTesting/Source/LoggingThread.c
+++ UnitTesting/LoggerUnitTesting/Source/LoggingThread.c
@@ -8,14 +8,10 @@
 
 double GetTime()
 {
-	/* Declaring structure for time */
 	struct timeval time;
 
-	/* Get current time and save it */
 	gettimeofday(&time, 0);
 
-	/* Combine the Secs with uSecs by typecasting
-     * Long Int to Double and return as 1 Double value */
 	return (double)(time.tv_sec)+(((double)(time.tv_usec))/1000000);
 }
 
