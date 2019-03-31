2a81685fbc77db05dbb560d901cc2400f626fc05
Raj <raj.lavingia@colorado.edu>
2019-03-30 19:59:19 -0600

    Socket_Client Header Added

---
+++ Includes/socket_client.h
@@ -0,0 +1,52 @@
+/*
+*@File:socket_client.h
+*@Description_File: Header File for socket client Sensor
+*@Author:Raj Lavingia and Yash Gupte
+*@Date: 03/28/2018
+*/
+
+
+//Includes are seperate because main.h cant be included for socket client
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/syscall.h>
+#include <netdb.h>
+#include <time.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+//Structures used for time
+struct timespec timespec_struct;
+struct timeval current_time;
+
+//Port Defined for connection between client and server
+#define PORT 8007
+
+//Temperature in specific unit
+#define tempc 25
+#define temf  75
+#define tempk 300
+
+//Check if temperatures status is what
+#define Temperature_OK					0x00
+#define Temperature_HIGH		0x01
+#define Temperature_LOW		0x02
+
+//Check lights statsu
+#define Night_Time			0x01
+#define Day_Time				0x02
+
+//Status of Temp or Lux by changing this particular Flag
+#define Temperature_Status			1
+#define Lux_Status				2
